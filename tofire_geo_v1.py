2f27f03ea17d7ea84d48b5bbead6bbfd93938f4a
Jackmoe <jackmoe@gmail.com>
2014-07-09 14:01:54 -0400

    Added v1 and v2 versions of updated scrape functions

---
+++ tofire_geo_v1.py
@@ -0,0 +1,80 @@
+import requests
+import csv
+import time
+import threading
+from bs4 import BeautifulSoup as bs
+from geopy.geocoders import GoogleV3
+import re
+import psycopg2
+import ipdb
+
+_URL = 'http://www.toronto.ca/fire/cadinfo/livecad.htm'
+
+def getincident():
+    response = requests.get(_URL)
+    r_code = 'Status = ' + str(response.status_code)
+    r_encoding = 'Encoding = ' + str(response.encoding)
+
+    soup = bs(response.text)
+    table = soup.find("table", class_="info")
+    cols = table.find_all('td')
+    num_cols = len(cols)
+    colslist = []
+    totalcols = 0
+
+    for col in cols:
+        colslist.append(col.string)
+        totalcols = len(colslist)
+
+    headers = colslist[0:7]
+    list_size = 8
+    i = 0
+    incidents = [colslist[i:i+list_size] for i in xrange(0, len(colslist), list_size)]
+
+    num_inci = len(incidents) # Get the number of incidents
+    added = time.strftime("%Y-%m-%d %H:%M:%S")
+    update = 'DB Updated @ ' + added
+
+    geolocator = GoogleV3()
+
+    conn = psycopg2.connect("dbname=jack user=jack")
+    cur = conn.cursor()
+
+    for incident in incidents[1:num_inci]:
+        prime = incident[0]
+        prime = prime[:-4]
+        cross = incident[1]
+        cross = cross.split('/', 1)
+        cross_one = cross[0]
+        cross_two = cross[1]
+        if prime and not prime.isspace() and cross_one and not cross_one.isspace():
+            lookup = str(prime) + ' at ' + str(cross_one) + ', ' + 'Toronto, ON'
+        elif prime and not prime.isspace() and cross_two and not cross_two.isspace():
+            lookup = str(prime) + ' at ' + str(cross_two) + ', ' + 'Toronto, ON'
+        elif cross_one and not cross_one.isspace() and cross_two and not cross_two.isspace():
+            lookup = str(cross_one) + ' at ' + str(cross_two) + ', ' + 'Toronto, ON'  
+        address, (latitude, longitude) = geolocator.geocode(lookup)
+        i[7] = i[7].replace("\n", "").replace("\r", "")
+        i[7] = i[7][:-2]
+        incident.append(address)
+        incident.append(latitude)
+        incident.append(longitude)
+        to_db = [i for i in incident]
+        cur.execute(
+            "INSERT INTO incidents (prime_street, cross_street, time, eid, etype, alarm, beat, units, address, latitude, longitude) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", to_db)
+    conn.commit()
+    #id_new_row = cur.fetchone()[0]
+    print update
+    #print "Last Row Inserted: " + id_new_row
+    # IT WORKS MAFACKA!
+    time.sleep(180)
+
+if __name__ == "__main__":
+    hw_thread = threading.Thread(target = getincident)
+    hw_thread.daemon = True
+    hw_thread.start()
+    try:
+        time.sleep(100000)
+    except KeyboardInterrupt:
+        print '\nGoodbye!'
+        
